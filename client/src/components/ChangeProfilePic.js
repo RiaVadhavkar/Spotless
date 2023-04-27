7cefebae7613f5aa2922fe3929cf30bee7cca783
antnguyen33 <ant33nguyen@gmail.com>
2023-04-26 22:42:37 -0400

    Image dropbox

--- client/src/components/ChangeProfilePic.js
+++ client/src/components/ChangeProfilePic.js
@@ -3,47 +3,53 @@ import { useContext, useState, useEffect } from "react";
 import { SessionContext } from "../App";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Dropzone from "./Dropzone";
 
 export default function ChangeProfilePic() {
-  // const handlePhoto = async (event) => {
-  //   event.preventDefault();
-  //   const api = "https://spotless-test-api.discovery.cs.vt.edu/";
-  //   const form = new FormData();
-  //   form.append("profile_photo", event.target.profile_photo.files[0]);
-  //   await axios
-  //     .post(api + "update/image", form, {
-  //       withCredentials: true,
-  //       headers: { Authorization: `Bearer ${sessionToken}` },
-  //     })
-  //     .then(function (response) {
-  //       console.log(response);
+  const {sessionToken, setSessionToken} = useContext(SessionContext);
 
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // };
+  const handlePhoto = async (event) => {
+    event.preventDefault();
+    const api = "https://spotless-test-api.discovery.cs.vt.edu/";
+    const form = new FormData();
+    var imageFile = document.querySelector("#file");
+    form.append("profile_photo", imageFile.files[0]);
+    await axios
+      .post(api + "update/image", form, {
+        withCredentials: true,
+        headers: { Authorization: `Bearer ${sessionToken}`, "Content-Type": "multipart/form-data" },
+      })
+      .then(function (response) {
+        console.log(response);
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div class="flex items-center justify-center w-full">
-      <form class="flex flex-col items-center justify-center w-full">
-        <label class="text-2xl mb-2">Change Profile Picture</label>
+      {/* <form class="flex flex-col items-center justify-center w-full"> */}
+        {/* <label class="text-2xl mb-2">Change Profile Picture</label> */}
         <div class="relative flex text-center border-2 border-dashed border-white rounded w-[12em] min-h-[12em] justify-center items-center">
-          <input
-            id="imageUpload"
+          {/* <input
+            id="file"
             type="file"
-            name="profile_photo"
+            name="file"
             required=""
             class="text-center opacity-0 cursor-pointer absolute z-10 w-full"
-            style={{ display: "none" }}
-            accept="image/*"
+            // style={{ display: "none" }}
+            accept="image/png, image/jpeg, image/jpg, image/gif"
+            onChange={handlePhoto}
             capture
           ></input>
           <label for="imageUpload" class="cursor-pointer block">
               Add Profile Picture Here
-          </label>
+          </label> */}
+          <Dropzone />
         </div>
-      </form>
+      {/* </form> */}
     </div>
   );
 }
